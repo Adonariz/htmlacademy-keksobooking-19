67b02ded331d42083e2b0a795b3a7367bab5457a
A Zhila <aleksej.zhila@gmail.com>
2020-03-05 00:21:37 +0300

    Что-то вносит

--- js/card.js
+++ js/card.js
@@ -54,41 +54,43 @@
     container.appendChild(fragment);
   };
 
+  var renderCard = function (advert) {
+    var сard = popupCardTemplate.cloneNode(true);
+    var avatar = сard.querySelector('.popup__avatar');
+    var title = сard.querySelector('.popup__title');
+    var address = сard.querySelector('.popup__text--address');
+    var price = сard.querySelector('.popup__text--price');
+    var type = сard.querySelector('.popup__type');
+    var capacity = сard.querySelector('.popup__text--capacity');
+    var time = сard.querySelector('.popup__text--time');
+    var featuresBlock = сard.querySelector('.popup__features');
+    var description = сard.querySelector('.popup__description');
+    var photos = сard.querySelector('.popup__photos');
+    var photo = photos.querySelector('.popup__photo');
+    var roomText = ['комната', 'комнаты', 'комнат'];
+    var guestText = ['гостя', 'гостей', 'гостей'];
+
+    avatar.src = advert.author.avatar;
+    title.textContent = advert.offer.title;
+    address.textContent = advert.offer.address;
+    price.textContent = advert.offer.price + '₽/ночь';
+    type.textContent = roomData[advert.offer.type].title;
+
+    if (advert.offer.guests === 0) {
+      capacity.textContent = advert.offer.rooms + ' ' + window.utils.pluralize(advert.offer.rooms, roomText) + ' без гостей';
+    }
+
+    capacity.textContent = advert.offer.rooms + ' ' + window.utils.pluralize(advert.offer.rooms, roomText) + ' для ' + advert.offer.guests + ' ' + window.utils.pluralize(advert.offer.guests, guestText);
+    time.textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
+    renderFeatures(advert.offer.features, featuresBlock);
+    description.textContent = advert.offer.description;
+    renderPhotos(advert.offer.photos, photos, photo);
+
+    return сard;
+  };
+
   window.card = {
     roomData: roomData,
-    render: function (advert) {
-      var сard = popupCardTemplate.cloneNode(true);
-      var avatar = сard.querySelector('.popup__avatar');
-      var title = сard.querySelector('.popup__title');
-      var address = сard.querySelector('.popup__text--address');
-      var price = сard.querySelector('.popup__text--price');
-      var type = сard.querySelector('.popup__type');
-      var capacity = сard.querySelector('.popup__text--capacity');
-      var time = сard.querySelector('.popup__text--time');
-      var featuresBlock = сard.querySelector('.popup__features');
-      var description = сard.querySelector('.popup__description');
-      var photos = сard.querySelector('.popup__photos');
-      var photo = photos.querySelector('.popup__photo');
-      var roomText = ['комната', 'комнаты', 'комнат'];
-      var guestText = ['гостя', 'гостей', 'гостей'];
-
-      avatar.src = advert.author.avatar;
-      title.textContent = advert.offer.title;
-      address.textContent = advert.offer.address;
-      price.textContent = advert.offer.price + '₽/ночь';
-      type.textContent = roomData[advert.offer.type].title;
-
-      if (advert.offer.guests === 0) {
-        capacity.textContent = advert.offer.rooms + ' ' + window.utils.pluralize(advert.offer.rooms, roomText) + ' без гостей';
-      }
-
-      capacity.textContent = advert.offer.rooms + ' ' + window.utils.pluralize(advert.offer.rooms, roomText) + ' для ' + advert.offer.guests + ' ' + window.utils.pluralize(advert.offer.guests, guestText);
-      time.textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
-      renderFeatures(advert.offer.features, featuresBlock);
-      description.textContent = advert.offer.description;
-      renderPhotos(advert.offer.photos, photos, photo);
-
-      return сard;
-    }
+    render: renderCard
   };
 })();
